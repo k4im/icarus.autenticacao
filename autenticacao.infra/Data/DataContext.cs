bfeb504f6c77477fe87849dc32c0b85db1b2f017
joao <joao@platon.com.br>
2023-09-06 13:42:10 -0300

    Adicionado conexão ao banco

--- autenticacao.infra/Data/DataContext.cs
+++ autenticacao.infra/Data/DataContext.cs
@@ -3,8 +3,21 @@ namespace autenticacao.infra.Data
 
     public class DataContext : IdentityDbContext<AppUser>
     {
-        public DataContext() : base(new DbContextOptionsBuilder().UseInMemoryDatabase("Data").Options)
+        public DataContext()
         { }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured) 
+            {
+                var conn = Environment.GetEnvironmentVariable("DB_CONNECTION");
+                var versionMysql = new MySqlServerVersion(new Version(8,10,31));
+                optionsBuilder.UseMySql(conn, versionMysql);
+            }
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
