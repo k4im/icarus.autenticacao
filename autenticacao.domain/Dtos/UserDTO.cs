691ca3b700e15b8764e7548be6bdb44300084004
k4im <contato.vitorsantos@hotmail.com>
2023-09-07 09:34:16 -0300

    Fix DTO user

--- autenticacao.domain/Dtos/UserDTO.cs
+++ autenticacao.domain/Dtos/UserDTO.cs
@@ -2,7 +2,7 @@ namespace autenticacao.domain.Dtos;
 
 public class UserDTO
 {
-    public int Id { get; set; }
+    public string Id { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
     public bool FlagDesativado { get; set; }
